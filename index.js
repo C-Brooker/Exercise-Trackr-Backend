e2db4b8a03a18fef090a5e6be5032160568466eb
C-Brooker <brookercharlie2003@gmail.com>
2022-11-06 00:29:33 +0000

    chore: refactored codes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +24,6 @@ const usersRouter = require("./routes.js/users");
 app.use("/exercises", exercisesRouter); //When going to the /exercises it will load everything in the exercisesRouter
 app.use("/users", usersRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
